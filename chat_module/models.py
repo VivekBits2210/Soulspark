00c6089545658b482e405d5b1a95fe9690104d5c
VivekBits2210 <viveknayak2210@gmail.com>
2023-03-13 03:15:19 -0400

    Relying on user as foreign key for profiles instead of UID

--- chat_module/models.py
+++ chat_module/models.py
@@ -1,15 +1,18 @@
 from django.db import models
 
 # Create your models here.
+from allauth import app_settings
 from django.db import models
 
 
 class UserProfile(models.Model):
-    UID = models.CharField(max_length=100, primary_key=True)
+    user = models.ForeignKey(app_settings.USER_MODEL, on_delete=models.CASCADE)
     age = models.PositiveIntegerField(null=True)
-    gender = models.CharField(max_length=10, choices=[('M', 'Male'), ('F', 'Female'), ('O', 'Other')])
-    level = models.CharField(max_length=50)
+    gender = models.CharField(max_length=10, choices=[('M', 'Male'), ('F', 'Female'), ('O', 'Other')], null=True)
+    gender_focus = models.CharField(max_length=10, choices=[('M', 'Male'), ('F', 'Female'), ('O', 'Other')], null=True)
+    level = models.CharField(max_length=50, default=1)
+    interests = models.CharField(max_length=250, null=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
-    REQUIRED_FIELDS = ['age', 'gender', 'level']
+    REQUIRED_FIELDS = ['username']
--- chat_module/views.py
+++ chat_module/views.py
@@ -1,4 +1,3 @@
-
 # Create your views here.
 import json
 from django.views.decorators.csrf import csrf_exempt
@@ -19,14 +18,18 @@ def index(request):
 @login_required
 def fetch_user(request):
     user = request.user
-    print(request.user)
     email = EmailAddress.objects.filter(user=user).first()
     social_account = SocialAccount.objects.filter(user=user).first()
-    profile = UserProfile.objects.filter(UID=social_account.uid).first()
+    profile_queryset = UserProfile.objects.filter(user=user)
 
-    # Create a dictionary with the combined user profile information
+    if not profile_queryset.exists():
+        profile = UserProfile.objects.create(user=user)
+        profile.save()
+    else:
+        profile = profile_queryset.first()
+
     user_profile = {
-        'UID': profile.UID,
+        'uid': social_account.uid,
         'username': user.username,
         'email': email.email,
         'age': profile.age,
@@ -35,6 +38,74 @@ def fetch_user(request):
     }
 
     return JsonResponse(user_profile, safe=False)
+
+
+@login_required
+@require_POST
+def post_age(request):
+    user = request.user
+    data = json.loads(request.body.decode('utf-8'))
+    age = int(data['age'])
+
+    profile_queryset = UserProfile.objects.filter(user=user)
+    if not profile_queryset.exists():
+        profile = UserProfile.objects.create(user=user)
+    else:
+        profile = profile_queryset.first()
+    profile.age = age
+    profile.save()
+    return JsonResponse({'status': 'ok'})
+
+
+@login_required
+@require_POST
+def post_gender(request):
+    user = request.user
+    data = json.loads(request.body.decode('utf-8'))
+    gender = data['gender']
+
+    profile_queryset = UserProfile.objects.filter(user=user)
+    if not profile_queryset.exists():
+        profile = UserProfile.objects.create(user=user)
+    else:
+        profile = profile_queryset.first()
+    profile.gender = gender
+    profile.save()
+    return JsonResponse({'status': 'ok'})
+
+
+@login_required
+@require_POST
+def post_gender_focus(request):
+    user = request.user
+    data = json.loads(request.body.decode('utf-8'))
+    gender_focus = data['gender_focus']
+
+    profile_queryset = UserProfile.objects.filter(user=user)
+    if not profile_queryset.exists():
+        profile = UserProfile.objects.create(user=user)
+    else:
+        profile = profile_queryset.first()
+    profile.gender_focus = gender_focus
+    profile.save()
+    return JsonResponse({'status': 'ok'})
+
+
+@login_required
+@require_POST
+def post_interests(request):
+    user = request.user
+    data = json.loads(request.body.decode('utf-8'))
+    interests = data['interests']
+
+    profile_queryset = UserProfile.objects.filter(user=user)
+    if not profile_queryset.exists():
+        profile = UserProfile.objects.create(user=user)
+    else:
+        profile = profile_queryset.first()
+    profile.interests = interests
+    profile.save()
+    return JsonResponse({'status': 'ok'})
 
 
 @login_required
