9adc7f6f9ff25122203839cd5e0eafafdeb3eb6f
VivekBits2210 <viveknayak2210@gmail.com>
2023-03-25 10:00:16 -0400

    Create user api

--- user_profiles/urls.py
+++ user_profiles/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from user_profiles.views import fetch_user_info, post_attribute
+from user_profiles.views import fetch_user_info, post_attribute, create_user
 
 urlpatterns = [
+    path("create-user", create_user.create_user, name="create_user"),
     path(
         "fetch-user-info", fetch_user_info.fetch_user_info, name="fetch_user_info"
     ),
