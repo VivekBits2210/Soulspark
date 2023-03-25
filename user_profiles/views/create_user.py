c1fd9a92e9a16b03834800b6ded63785d93122bd
VivekBits2210 <viveknayak2210@gmail.com>
2023-03-25 10:03:42 -0400

    Actually added create user api

---
+++ user_profiles/views/create_user.py
@@ -0,0 +1,9 @@
+from django.http import JsonResponse
+from rest_framework import status
+from rest_framework.decorators import api_view
+
+
+@api_view(["POST"])
+def create_user(request):
+    request_dict = request.data
+    return JsonResponse(request_dict, safe=False, status=status.HTTP_200_OK)
