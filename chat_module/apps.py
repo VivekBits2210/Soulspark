d971c4138a95795e80c6bc2faaccaea736a4ec14
ravaan <arpit.agarwal181@gmail.com>
2023-04-01 19:55:21 +0530

    add celery configurations

--- chat_module/apps.py
+++ chat_module/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ChatModuleConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "chat_module"
+
+    def ready(self):
+        import soulspark_backend.celery_app
