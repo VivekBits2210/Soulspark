19f44313cd4a1153266638b34fb7d150110f797b
VivekBits2210 <viveknayak2210@gmail.com>
2023-04-03 12:24:47 -0400

    More static images

--- soulspark_backend/views/fill_db.py
+++ soulspark_backend/views/fill_db.py
@@ -10,13 +10,11 @@ from user_profiles.models import UserProfile
 
 @api_view(["GET"])
 def fill_db(request):
-    image_path = os.path.join("static", "trial.jpg")
-    with open(image_path, "rb") as f:
-        image_content1 = f.read()
-
-    image_path = os.path.join("static", "trial1.jpg")
-    with open(image_path, "rb") as f:
-        image_content2 = f.read()
+    image_content = []
+    for file in ["trial","trial1","Nicole","Sheanguang"]:
+        image_path = os.path.join("static", f"{file}.jpg")
+        with open(image_path, "rb") as f:
+            image_content.append(f.read())
 
     profile1_data = {
         "name": "Nicole",
@@ -27,7 +25,7 @@ def fill_db(request):
         "physical_attributes": {"eyes": "blue"},
         "profession": "Air hostess",
         "profile_image": SimpleUploadedFile(
-            "Nicole.jpg", image_content1, content_type="image/jpeg"
+            "Nicole.jpg", image_content[0], content_type="image/jpeg"
         ),
         "bio": "Lorem ipsum",
     }
@@ -45,18 +43,52 @@ def fill_db(request):
         },
         "profession": "Secretary",
         "profile_image": SimpleUploadedFile(
-            "Carla.jpg", image_content2, content_type="image/jpeg"
+            "Carla.jpg", image_content[1], content_type="image/jpeg"
+        ),
+        "bio": "Lorem ipsum",
+    }
+    profile3_data = {
+        "name": "Aurelia",
+        "gender": "F",
+        "age": 19,
+        "interests": "hiking and jogging",
+        "favorites": {"color": "blue", "food": "sushi"},
+        "physical_attributes": {
+            "eyes": "blue",
+            "hair": "blonde",
+            "skin": "fair",
+            "figure": "slim",
+        },
+        "profession": "Waitress",
+        "profile_image": SimpleUploadedFile(
+            "Nicole.jpg", image_content[2], content_type="image/jpeg"
+        ),
+        "bio": "Bio for Nicole",
+    }
+    profile4_data = {
+        "name": "Shenguang",
+        "gender": "F",
+        "age": 24,
+        "interests": "hiking and jogging",
+        "favorites": {"color": "blue", "food": "sushi"},
+        "physical_attributes": {
+            "eyes": "blue",
+            "hair": "blonde",
+            "skin": "fair",
+            "figure": "slim",
+        },
+        "profession": "Secretary",
+        "profile_image": SimpleUploadedFile(
+            "Carla.jpg", image_content[3], content_type="image/jpeg"
         ),
         "bio": "Lorem ipsum",
     }
 
     # Create the two BotProfile instances using the data
     BotProfile.objects.all().delete()
-    UserProfile.objects.all().delete()
-    profile1 = BotProfile(**profile1_data)
-    profile2 = BotProfile(**profile2_data)
+    BotProfile(**profile1_data).save()
+    BotProfile(**profile2_data).save()
+    BotProfile(**profile3_data).save()
+    BotProfile(**profile4_data).save()
 
-    # Save the profiles to the database
-    profile1.save()
-    profile2.save()
     return JsonResponse({"status": "ok"})
