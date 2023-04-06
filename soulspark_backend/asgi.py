7c9cf401f926e98d7200be4944b2ce7a154c4450
VivekBits2210 <viveknayak2210@gmail.com>
2023-04-06 08:15:21 -0400

    Quick fixes

--- soulspark_backend/asgi.py
+++ soulspark_backend/asgi.py
@@ -11,13 +11,13 @@ import os
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-from chat_module import routing
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "soulspark_backend.settings")
 
 django_asgi_app = get_asgi_application()
 
+from chat_module import routing
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
