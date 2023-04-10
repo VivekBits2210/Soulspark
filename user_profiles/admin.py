cdab9bf27040ec74daa8221a7f6eb9f5730fa786
VivekBits2210 <viveknayak2210@gmail.com>
2023-04-10 15:52:04 -0400

    Register user feedback table

--- user_profiles/admin.py
+++ user_profiles/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from user_profiles.models import User, UserProfile
+from user_profiles.models import User, UserProfile, UserFeedback
 
 admin.site.register(User)
 admin.site.register(UserProfile)
+admin.site.register(UserFeedback)
 # Register your models here.
