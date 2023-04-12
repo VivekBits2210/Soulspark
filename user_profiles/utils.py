81fd0b96d7c84eeeb8161c4b4b720db40fcfccbc
VivekBits2210 <viveknayak2210@gmail.com>
2023-04-12 18:32:50 -0400

    Non-CBC encryption part 2

--- user_profiles/utils.py
+++ user_profiles/utils.py
@@ -12,22 +12,20 @@ from dotenv import dotenv_values
 SALT = dotenv_values(".env")['SALT']
 
 
-def encrypt_email(email, key=SALT):
+def encrypt_email(data, key):
     key = key.encode("utf-8")
-    iv = get_random_bytes(AES.block_size)
-    cipher = AES.new(key, AES.MODE_CBC, iv)
-    padded_email = pad(email.encode("utf-8"), AES.block_size)
-    ciphertext = cipher.encrypt(padded_email)
-    encrypted_email = iv + ciphertext
-    return encrypted_email
+    cipher = AES.new(key, AES.MODE_ECB)
+    encrypted_data = cipher.encrypt(pad(data.encode('utf-8'), AES.block_size))
+    return base64.b64encode(encrypted_data).decode('utf-8')
 
 
-def decrypt_email(email, key=SALT):
+def decrypt_email(email, key):
     key = key.encode("utf-8")
     encrypted_data = base64.b64decode(email)
     cipher = AES.new(key, AES.MODE_ECB)
     decrypted_data = unpad(cipher.decrypt(encrypted_data), AES.block_size)
     return decrypted_data.decode('utf-8')
+
     # key = key.encode("utf-8") if isinstance(key, str) else key
     # ciphertext = base64.b64decode(ciphertext)
     # iv = ciphertext[: AES.block_size]
