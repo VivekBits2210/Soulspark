b120a2213d97fca754c4dec098edc374ae554cec
VivekBits2210 <viveknayak2210@gmail.com>
2023-04-12 21:35:11 -0400

    Return user if already exists

--- user_profiles/views/create_user.py
+++ user_profiles/views/create_user.py
@@ -1,7 +1,10 @@
 from django.core.exceptions import ValidationError
+from django.forms import model_to_dict
 from django.http import JsonResponse
 from rest_framework import status
 from rest_framework.decorators import api_view
+
+from user_profiles.models import User
 from user_profiles.serializers import UserSerializer
 from user_profiles.utils import decrypt_email
 
@@ -12,6 +15,8 @@ def create_user(request):
 
     try:
         decrypted_email = decrypt_email(request_dict["email"])
+        if User.objects.filter(email=decrypted_email):
+            return JsonResponse(model_to_dict(User.objects.get(email=decrypted_email)), status=status.HTTP_200_OK)
     except KeyError:
         return JsonResponse(
             {"error": "email parameter missing"}, status=status.HTTP_400_BAD_REQUEST
